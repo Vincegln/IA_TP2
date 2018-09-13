ffa78839789dd0eaf5d98f7cf513d6d528e0fef9
Vincent <vincent.guillon@hotmail.fr>
2018-09-13 16:20:10 -0400

    Cleanup

--- Path.h
+++ Path.h
@@ -42,7 +42,7 @@ public:
 
 
   //returns the current waypoint
-  Vector2D    CurrentWaypoint()const{assert(curWaypoint != NULL); return *curWaypoint;}
+  Vector2D    CurrentWaypoint()const{assert(curWaypoint != m_WayPoints.end); return *curWaypoint;}
 
   //returns true if the end of the list has been reached
   bool        Finished(){return !(curWaypoint != m_WayPoints.end());}
